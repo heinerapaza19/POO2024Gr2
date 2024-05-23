764227c17a58f39ee5e8c8478bc74493949f449b
heinerapaza19 <heiner1408apz@gmail.com>
2024-05-22 23:11:34 -0500

    tareas

--- SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/repositorio/ClienteRepository.java
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/repositorio/ClienteRepository.java
@@ -1,7 +1,10 @@
 
 package pe.edu.upeu.syscenterlife.repositorio;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upeu.syscenterlife.modelo.Cliente;
 
@@ -9,5 +12,10 @@ import pe.edu.upeu.syscenterlife.modelo.Cliente;
 public interface ClienteRepository extends 
         JpaRepository<Cliente, String>{
     
+    @Query(value = "SELECT * FROM cliente WHERE nombrers like :nombre", nativeQuery = true)
+    List<Cliente> findByNombre(@Param(value = "nombre") String nombre);
+    
+    @Query(value = "SELECT * FROM cliente WHERE nombrers LIKE :nombre", nativeQuery = true)
+    List<Cliente> findByNombre(@Param(value = "nombre") String nombre);
     
 }
